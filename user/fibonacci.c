eaad290ba48fd0e29a2cb85828d560a3de17f67f
veerprasad05 <veerprasad@gmail.com>
2025-04-11 01:16:30 -0400

    fibonacci modified for freezer

--- user/fibonacci.c
+++ user/fibonacci.c
@@ -1,35 +1,52 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <sched.h>
 #include <errno.h>
+#include <string.h>
 
 unsigned long long fib(unsigned long long n)
 {
-	if (n <= 1)
-		return n;
-	else
-		return fib(n - 1) + fib(n - 2);
+    if (n <= 1)
+        return n;
+    else
+        return fib(n - 1) + fib(n - 2);
 }
 
 int main(int argc, char **argv)
 {
-	struct sched_param params = {0};
-	int ret;
-	unsigned long long n;
+    struct sched_param params = {0};
+    int ret;
+    unsigned long long n;
+    int policy = SCHED_FREEZER; // Default to FREEZER
 
-	ret = sched_setscheduler(0, 7, &params);
-	if (ret) {
-		perror("Error\n");
-		fprintf(stderr, "Freezer scheduling policy does not exist\n");
-		exit(1);
-	}
+    // Parse command line arguments
+    if (argc < 2 || argc > 3) {
+        fprintf(stderr, "Usage: ./fibonacci [number] [policy]\n");
+        fprintf(stderr, "Policies: 1=FIFO, 7=FREEZER, 8=HEATER\n");
+        exit(1);
+    }
 
-	if (argc != 2) {
-		fprintf(stderr, "Usage: ./fibonacci [number]\n");
-		exit(1);
-	}
+    // Get the Fibonacci number to calculate
+    n = atoi(argv[1]);
 
-	n = atoi(argv[1]);
-	return fib(n);
+    // Get the scheduling policy if provided
+    if (argc == 3) {
+        policy = atoi(argv[2]);
+        if (policy != SCHED_FIFO && policy != SCHED_FREEZER && policy != 8) {
+            fprintf(stderr, "Invalid policy. Use 1 for FIFO, 7 for FREEZER, or 8 for HEATER\n");
+            exit(1);
+        }
+    }
+
+    // Set the scheduling policy
+    ret = sched_setscheduler(0, policy, &params);
+    if (ret) {
+        perror("Error setting scheduler policy");
+        fprintf(stderr, "Failed to set scheduling policy %d: %s\n", 
+                policy, strerror(errno));
+        exit(1);
+    }
+
+    // Calculate the Fibonacci number
+    return fib(n);
 }
